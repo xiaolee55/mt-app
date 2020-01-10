2f116c0826271e3d854e9c50b91d30643005ed25
1084235321@qq.com <1084235321@qq.com>
2020-01-10 15:13:02 +0800

    完成购物车模块的开发

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ import users from './interface/users'
 import geo from './interface/geo'
 import search from './interface/search'
 import categroy from './interface/categroy'
+import cart from './interface/cart'
 
 const app = new Koa()
 
@@ -57,7 +58,8 @@ async function start () {
   app.use(geo.routes()).use(geo.allowedMethods())
   app.use(search.routes()).use(search.allowedMethods())
   app.use(categroy.routes()).use(categroy.allowedMethods())
-  
+  app.use(cart.routes()).use(cart.allowedMethods())
+
   app.use((ctx) => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
